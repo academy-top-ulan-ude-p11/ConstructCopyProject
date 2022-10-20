cb1723de5fe043383c0e83690871eb87382b4e96
GitStudent2022 <torelgame@mail.ru>
2022-10-20 13:11:27 +0300

    20-10-99

--- ConstructCopyProject/ConstructCopyProject.cpp
+++ ConstructCopyProject/ConstructCopyProject.cpp
@@ -1,4 +1,5 @@
-﻿#include <iostream>
+﻿#define _CRT_SECURE_NO_WARNINGS
+#include <iostream>
 
 class User
 {
@@ -37,6 +38,41 @@ void Func(MyClass obj3)
     std::cout << "func is work\n";
 }
 
+class MyString
+{
+    char* str;
+public:
+    MyString(char ch) = delete;
+
+    explicit MyString(int size)
+    {
+        str = new char[size];
+        str[size - 1] = '\0';
+    }
+
+    MyString(const char* str)
+    {
+        this->str = new char[strlen(str) + 1];
+        strcpy(this->str, str);
+    }
+
+    //MyString(const MyString& s) = delete;
+    //MyString operator=(const MyString& s) = delete;
+
+    friend std::ostream& operator<<(std::ostream& out, const MyString& s)
+    {
+        out << s.str;
+        return out;
+    }
+
+    int Size() const
+    {
+        return strlen(this->str) + 1; //sizeof(this->str);
+    }
+
+};
+
+
 MyClass FuncReturn()
 {
     MyClass obj4;
@@ -45,10 +81,10 @@ MyClass FuncReturn()
 
 int main()
 {
-    int n = 10;
+    /*int n = 10;
     int m = n;
 
-    MyClass obj;
+    MyClass obj;*/
 
     // 1
     //MyClass obj2 = obj;
@@ -59,4 +95,15 @@ int main()
     // 3
     //obj = FuncReturn();
 
+
+    // MyString
+    MyString s1(10);
+    std::cout << s1 << " " << s1.Size() << "\n";
+
+    MyString s2("Hello");
+    std::cout << s2 << " " << s2.Size() << "\n"
+
+
+    //MyString s3('A');
+    //std::cout << s3 << " " << s3.Size() << "\n";
 }
